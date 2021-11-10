7ee4216ee33807abee9023b40addfa9781a4ac95
Rdawg028 <verdouwr1827@my.uwstout.edu>
2021-11-10 10:15:11 -0600

    Update Range2.cs

--- Assets/Range2.cs
+++ Assets/Range2.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Range2 : MonoBehaviour
 {
+
+    GameObject Player2;
+    Player2Punching Player2Var;
+    GameObject tmpHealth;
+    Slider HealthBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player2 = GameObject.Find("Player2");
+        Player2Var = Player2.GetComponent<Player2Punching>();
+
+        tmpHealth = GameObject.Find("HealthBar");
+        HealthBar = tmpHealth.GetComponent<Slider>();
+        HealthBar.value = Player2Var.p1Health;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.gameObject.name == "Player1")
+        {
+            Player2Var.p1Health = Player2Var.TakeRangeDamage(Player2Var.p1Health);
+            Debug.Log("Fist Hit");
+            HealthBar.value = Player2Var.p1Health;
+            Debug.Log("health bar");
+
+        }
     }
 }
