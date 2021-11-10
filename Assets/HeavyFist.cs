6398e99990342b8cd8f24c63e8663fcc75323400
Rdawg028 <verdouwr1827@my.uwstout.edu>
2021-11-10 09:36:32 -0600

    new script heavy attack

--- Assets/HeavyFist.cs
+++ Assets/HeavyFist.cs
@@ -3,35 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Fist : MonoBehaviour
+public class HeavyFist : MonoBehaviour
 {
 
     GameObject Player2;
     Player2Punching Player2Var;
     GameObject tmpHealth;
     Slider HealthBar;
-    
 
     // Start is called before the first frame update
     void Start()
     {
-       Player2 = GameObject.Find("Player2");
-       Player2Var = Player2.GetComponent<Player2Punching>();
+        Player2 = GameObject.Find("Player2");
+        Player2Var = Player2.GetComponent<Player2Punching>();
 
-       tmpHealth = GameObject.Find("HealthBar");
-       HealthBar = tmpHealth.GetComponent<Slider>();
-       HealthBar.value = Player2Var.p1Health;
+        tmpHealth = GameObject.Find("HealthBar");
+        HealthBar = tmpHealth.GetComponent<Slider>();
+        HealthBar.value = Player2Var.p1Health;
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.gameObject.name == "Player1")
         {
-            Player2Var.p1Health = Player2Var.TakeDamageLight(Player2Var.p1Health);
+            Player2Var.p1Health = Player2Var.TakeDamageHeavy(Player2Var.p1Health);
             Debug.Log("Fist Hit");
             HealthBar.value = Player2Var.p1Health;
             Debug.Log("health bar");
-            
+
         }
     }
 }
