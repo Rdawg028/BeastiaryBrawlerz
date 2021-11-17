fae302c2cf93717d0cffb4804f58f7f20830ce13
Harrison-Stone <stoneh8721@my.uwstout.edu>
2021-11-17 11:19:02 -0600

    More edits

--- Assets/HeavyFist.cs
+++ Assets/HeavyFist.cs
@@ -15,20 +15,20 @@ public class HeavyFist : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Player2 = GameObject.Find("Player2");
+        Player2 = GameObject.FindWithTag("Player 2");
         Player2Var = Player2.GetComponent<Player2Punching>();
 
         tmpHealth = GameObject.Find("HealthBar");
         HealthBar = tmpHealth.GetComponent<Slider>();
         HealthBar.value = Player2Var.p1Health;
 
-        Player1 = GameObject.Find("Player1");
+        Player1 = GameObject.FindWithTag("Player 1");
         anim = Player1.GetComponent<Animator>();
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.gameObject.tag == "Players")
+        if (collision.collider.gameObject.tag == "Player 1")
         {
             Player2Var.p1Health = Player2Var.TakeDamageHeavy(Player2Var.p1Health);
             Debug.Log("Fist Hit");
