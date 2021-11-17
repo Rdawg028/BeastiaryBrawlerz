96181c7338de677ebc5a2cfadb24078a628aeb99
Harrison-Stone <stoneh8721@my.uwstout.edu>
2021-11-17 10:56:46 -0600

    Morfe edits

--- Assets/Heavy1.cs
+++ Assets/Heavy1.cs
@@ -17,14 +17,14 @@ public class Heavy1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Player1 = GameObject.Find("Player1");
+        Player1 = GameObject.FindWithTag("Player 1");
         Player1Var = Player1.GetComponent<Player1Punching>();
 
         tmpHealth = GameObject.Find("HealthBar2");
         HealthBar = tmpHealth.GetComponent<Slider>();
         HealthBar.value = Player1Var.p2Health;
 
-        Player2 = GameObject.Find("Player2");
+        Player2 = GameObject.FindWithTag("Player 2");
         scr = Player2.GetComponent<Player2Punching>();
 
         anim = Player2.GetComponent<Animator>();
@@ -34,7 +34,7 @@ public class Heavy1 : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.gameObject.tag == "Players")
+        if (collision.collider.gameObject.tag == "Player 2")
         {
             Player1Var.p2Health = scr.TakeDamageHeavy(Player1Var.p2Health);
             Debug.Log("Fist Hit");
