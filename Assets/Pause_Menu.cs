f781572a69769bacf5e01752bc919a5c26eb1f95
Harrison-Stone <stoneh8721@my.uwstout.edu>
2021-11-22 09:27:27 -0600

    Minor edits

--- Assets/Pause_Menu.cs
+++ Assets/Pause_Menu.cs
@@ -1,36 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause_Menu : MonoBehaviour
 {
     
     GameObject timer;
     TimerScript isRunning;
+    GameObject menu;
 
     private void Start()
     {
         timer = GameObject.Find("Timer");
         isRunning = timer.GetComponent<TimerScript>();
+        menu = GameObject.Find("Pause");
     }
     void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
+        {
+            isRunning.timeRunning = !isRunning.timeRunning;
             
-                isRunning.timeRunning = !isRunning.timeRunning;
+        }
+            
+               
             
             
     }
 
     void OnGUI()
     {
+        //make a panel
+        //have it normally disable, enable on key press
+        //as many buttons as you want
         if (!isRunning.timeRunning&&isRunning.timeRemaining>0)
         {
-            GUILayout.Label("Game is paused!");
+
+            menu.SetActive(!isRunning.timeRunning);
             if (GUILayout.Button("Click me to unpause"))
             {
                 isRunning.timeRunning = !isRunning.timeRunning;
+                menu.SetActive(!isRunning.timeRunning);
             }
+            
                 
         }
     }
