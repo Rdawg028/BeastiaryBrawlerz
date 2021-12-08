1064eca78d7224d4092a9b7a49e8cc032952c42c
Harrison-Stone <stoneh8721@my.uwstout.edu>
2021-12-08 11:02:32 -0600

    Stuff

---
+++ Assets/Scenes/CharacterSelector/IronMaker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronMaker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        void OnCollisonEnter2D(Collision2D select)
+        {
+            if (false)
+            {
+
+            }
+        }
+    }
+}
